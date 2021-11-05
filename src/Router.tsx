93ec61541592946b943c603cfb148bcac6202afa
taweewatK <taweewat.k@20scoops.net>
2021-11-05 15:39:06 +0700

    feat: single quote

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
-import { FC } from "react";
-import { Route, Switch } from "react-router-dom";
-import Pokemon from "./pages/Pokemon/index";
+import { FC } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Pokemon from './pages/Pokemon/index';
 
 const Router: FC = () => {
   // return;
